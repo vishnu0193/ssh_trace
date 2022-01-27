8d00dcea160318071d8993a59942973e84bab432
vishnu0193 <vishnuvardhan231@gmail.com>
2022-01-27 23:34:30 +0530

    Commenting the unwanted vars

--- infra/nodes/variables.tf
+++ infra/nodes/variables.tf
@@ -1,19 +1,19 @@
-variable "instance_tag" {
-    type = string
-    default = "alphaserver"
-}
+# variable "instance_tag" {
+#     type = string
+#     default = "alphaserver"
+# }
 
-variable "instance_tag_client" {
-    type = string
-    default = "alpha-client-1"
-}
+# variable "instance_tag_client" {
+#     type = string
+#     default = "alpha-client-1"
+# }
 
 
-variable "instance_type"{
-    type = string
-    description = "(optional) describe your variable"
-    default = "t2.micro"
-}
+# variable "instance_type"{
+#     type = string
+#     description = "(optional) describe your variable"
+#     default = "t2.micro"
+# }
 
 variable "vpc_tag" {
     type = string
@@ -21,11 +21,11 @@ variable "vpc_tag" {
     default = "ssh_trace"
 }
 
-variable "key_name" {
-    type = string
-    description = "(optional) describe your variable"
-    default = "ssh_trace"
-}
+# variable "key_name" {
+#     type = string
+#     description = "(optional) describe your variable"
+#     default = "ssh_trace"
+# }
 
 variable "vpc_name" {
     type = string
@@ -33,8 +33,8 @@ variable "vpc_name" {
     default = "value"
 }
 
-variable "scope" {
-    type = string
-    description = "(optional) describe your variable"
-    default = "ssh_tracing"
-}
+# variable "scope" {
+#     type = string
+#     description = "(optional) describe your variable"
+#     default = "ssh_tracing"
+# }
