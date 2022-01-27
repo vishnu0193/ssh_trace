a35759b50fe86c586892c641866ea12df0894f89
vishnu0193 <vishnuvardhan231@gmail.com>
2022-01-28 00:01:19 +0530

    Calling the initial networking module for vpc creation

--- infra/nodes/nodes.tf
+++ infra/nodes/nodes.tf
@@ -6,29 +6,3 @@ module "vpc" {
     vpc_tag = var.vpc_tag    
 }
 
-module "sg" {
-source = "../../modules/sgroups" 
-vpc_id = module.vpc.vpc_ssh_id
-depends_on = [
-  module.vpc
-]
-}
-
-
-module "vms-server" {
-    source = "../../modules/ec2"
-    instance_type = var.instance_type
-    instance_tag  = var.instance_tag
-    //security_groups_name = [module.sg.vpc_sg_id]
-    scope  = var.scope
-    key_name = var.key_name
-}
-
-module "vms-client" {
-    source = "../../modules/ec2"
-    instance_type = var.instance_type
-    instance_tag  = var.instance_tag_client
-    //security_groups_name = [module.sg.vpc_sg_id]
-    scope  = var.scope
-    key_name = var.key_name
-}
