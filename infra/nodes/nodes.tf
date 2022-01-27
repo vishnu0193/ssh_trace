a4e07766c64b9d550f60aa17d16c3dae7e7bdbf7
vishnu0193 <vishnuvardhan231@gmail.com>
2022-01-28 05:28:17 +0530

    Adding the terraform provisioners for deploying loggn script and client infra implementation

--- infra/nodes/nodes.tf
+++ infra/nodes/nodes.tf
@@ -39,3 +39,123 @@ module "vms-client-2" {
     scope  = var.scope
     key_name = var.key_name
 }
+
+data "aws_instance" "server" {
+filter {
+    name   = "tag:Name"
+    values = ["alphaserver"]
+  }
+  depends_on = [
+    module.vms-server
+  ]
+}
+
+data "aws_instance" "client1" {
+filter {
+    name   = "tag:Name"
+    values = ["alpha-client-1"]
+  }
+  depends_on = [
+    module.vms-client-1
+  ]
+}
+resource "null_resource" "ec2-ssh-connection" {
+  provisioner "file" {
+    source      = "../../src"
+    destination = "/home/ubuntu/"
+    connection {
+      host        = data.aws_instance.server.public_ip
+      type        = "ssh"
+      port        = 22
+      user        = "ubuntu"
+      private_key = "${file("~/xxx/xxx.pem")}"
+      timeout     = "1m"
+      agent       = false
+    }
+  }
+}
+resource "null_resource" "ec2-ssh-connection-script-deploy" {
+    triggers = {
+    always_run = timestamp()
+  }
+  provisioner "remote-exec" {
+    connection {
+      host        = data.aws_instance.server.public_ip
+      type        = "ssh"
+      port        = 22
+      user        = "ubuntu"
+      private_key = "${file("~/xxx/xxx.pem")}"
+      timeout     = "1m"
+      agent       = false
+    }
+  inline = [
+      "chmod +x /home/ubuntu/src/*",
+      "crontab -r",
+      "rm -r /home/ubuntu/ssh_success.log",
+      "(crontab -l 2>/dev/null; echo '* * * * * /home/ubuntu/src/ssh_trace_success.sh >> /home/ubuntu/ssh_success.log') | crontab -",
+    ]
+  }
+}
+resource "null_resource" "ec2-ssh-key-copy-client-1" {
+  provisioner "file" {
+    source      = "~/xxxx/xxx.pem"
+    destination = "/home/ubuntu/ssh.pem"
+    connection {
+      host        = data.aws_instance.client1.public_ip
+      type        = "ssh"
+      port        = 22
+      user        = "ubuntu"
+      private_key = "${file("~/xxxx/xxx.pem")}"
+      timeout     = "1m"
+      agent       = false
+    }
+  }
+}
+
+resource "null_resource" "ec2-ssh-connection-client-1" {
+ triggers = {
+    always_run = timestamp()
+  }
+  provisioner "remote-exec" {
+    connection {
+      host        = data.aws_instance.client1.public_ip
+      type        = "ssh"
+      port        = 22
+      user        = "ubuntu"
+      private_key = "${file("~/xxx/xxx.pem")}"
+      timeout     = "1m"
+      agent       = false
+    }
+  inline = [
+      "cd /home/ubuntu",
+      "chmod 400 ssh.pem",
+      "ssh -i /home/ubuntu/ssh.pem ubuntu@${data.aws_instance.server.private_ip} -f 'sleep 2s ; exit'",
+    ]
+  }
+}
+
+resource "time_sleep" "wait_60" {
+  create_duration = "60s"
+}
+
+resource "null_resource" "display-output" {
+  triggers = {
+    always_run = timestamp()
+  }
+  depends_on = [time_sleep.wait_60]
+  provisioner "remote-exec" {
+    connection {
+      host        = data.aws_instance.server.public_ip
+      type        = "ssh"
+      port        = 22
+      user        = "ubuntu"
+      private_key = "${file("~/xx/xxx.pem")}"
+      timeout     = "1m"
+      agent       = false
+    }
+  inline = [
+      "echo 'sleeping for 10 s' ",
+      "cat /home/ubuntu/ssh_success.log"
+    ]
+  }
+}
